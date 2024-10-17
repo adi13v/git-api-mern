a049aacf62837d6e01515c82af9a389d9d7933ec
Aditya Verma <aditya1310v@gmail.com>
2024-10-17 18:11:22 +0530

    Initiating the Project

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <aside className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r
+    h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 border border-gray-100
+     '>Hello</aside>
+  )
+}
+
+export default Sidebar
